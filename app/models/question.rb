d71a4bc552d490c5f184551ddd3d1e4620e6e842
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-10-10 13:08:34 +0000

    Change update callback

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,18 +6,36 @@ class Question < ApplicationRecord
 
 	after_create do
 		question_tags = self.tag_body.scan(/#\w+/)
-		question_tags.uniq.map do |hashtag|
+		add_tags(question_tags)
+	end
+
+	before_update do
+		question_tags = self.tag_body.scan(/#\w+/)
+		update_tags(question_tags)
+	end
+
+	private
+
+	def update_tags(tags)
+		tags_to_remove(tags).each(&:delete)
+		add_tags(tags_to_insert(tags))
+	end
+
+	# For each tag, find or create it, and add to the question itself.
+	def add_tags(tags)
+		tags.uniq.map do |hashtag|
 			tag = Hashtag.find_or_create_by(name: hashtag.downcase.delete('#'))
 			self.hashtags << tag
 		end
 	end
 
-	before_update do
-		self.hashtags.clear # delete all and add again
-		question_tags = self.tag_body.scan(/#\w+/)
-		question_tags.uniq.map do |hashtag|
-			tag = Hashtag.find_or_create_by(name: hashtag.downcase.delete('#'))
-			self.hashtags << tag
-		end
+	# Get tags to be inserted.
+	def tags_to_insert(tags)
+		tags - self.hashtags.map(&:name)
+	end
+
+	# Get tags to be removed.
+	def tags_to_remove(tags)
+		self.hashtags.where.not(name: tags).to_a
 	end
 end
